9355734d98485a3c54cbc93a61fd6c2ac7ac36ea
Osneue <143680960+Osneue@users.noreply.github.com>
2024-01-10 19:20:58 +0800

    Add Home & Category & Checkout component (empty)

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react'
+import Category from './Category'
+import Checkout from './Checkout'
+import Home from './Home'
+
+const PAGE_HOME = 'HOME'
+const PAGE_CATEGORY = 'CATEGORY'
+const PAGE_CHECKOUT = 'CHECKOUT'
+
 const App = () => {
-  return <div>App</div>
+  const [page, setPage] = useState(PAGE_HOME)
+
+  return (
+    <>
+      {page === PAGE_HOME && <Home />}
+      {page === PAGE_CATEGORY && <Category />}
+      {page === PAGE_CHECKOUT && <Checkout />}
+    </>
+  )
 }
 export default App
