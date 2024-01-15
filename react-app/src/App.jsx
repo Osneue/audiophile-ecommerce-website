5dc348d02ece0758e8263f8ed92e52ef4c92ec4b
Osneue <143680960+Osneue@users.noreply.github.com>
2024-01-15 13:27:27 +0800

    Jump to top when routing

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,27 +4,50 @@ import './App.css'
 import Category from './pages/category'
 import Home from './pages/home'
 
+const ScrollToTop = () => {
+  window.scrollTo(0, 0)
+}
+
 const App = () => {
   const [menu, setMenu] = useState(false)
 
   return (
     <Routes>
-      <Route path='/' element={<Home menu={menu} setMenu={setMenu} />} />
+      <Route
+        path='/'
+        element={
+          <>
+            <Home menu={menu} setMenu={setMenu} />
+            <ScrollToTop />
+          </>
+        }
+      />
       <Route
         path='/headphones'
         element={
-          <Category category='HEADPHONES' menu={menu} setMenu={setMenu} />
+          <>
+            <Category category='HEADPHONES' menu={menu} setMenu={setMenu} />
+            <ScrollToTop />
+          </>
         }
       />
       <Route
         path='/earphones'
         element={
-          <Category category='EARPHONES' menu={menu} setMenu={setMenu} />
+          <>
+            <Category category='EARPHONES' menu={menu} setMenu={setMenu} />
+            <ScrollToTop />
+          </>
         }
       />
       <Route
         path='/speakers'
-        element={<Category category='SPEAKERS' menu={menu} setMenu={setMenu} />}
+        element={
+          <>
+            <Category category='SPEAKERS' menu={menu} setMenu={setMenu} />
+            <ScrollToTop />
+          </>
+        }
       />
     </Routes>
   )
